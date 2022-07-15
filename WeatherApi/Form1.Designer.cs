daf4e9a767dc32596968f9ea6f41f9938b6179f4
cancelik-dev <cancelikdev@gmail.com>
2022-07-15 20:25:56 +0300

    WeatherApi
    WeatherApi

---
+++ WeatherApi/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace WeatherApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl_temp = new System.Windows.Forms.Label();
+            this.lbl_usak = new System.Windows.Forms.Label();
+            this.lbl_state = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_rzg = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbl_nem = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_ygs = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_temp
+            // 
+            this.lbl_temp.AutoSize = true;
+            this.lbl_temp.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_temp.Location = new System.Drawing.Point(265, 137);
+            this.lbl_temp.Name = "lbl_temp";
+            this.lbl_temp.Size = new System.Drawing.Size(78, 35);
+            this.lbl_temp.TabIndex = 0;
+            this.lbl_temp.Text = "00.00";
+            // 
+            // lbl_usak
+            // 
+            this.lbl_usak.AutoSize = true;
+            this.lbl_usak.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_usak.Location = new System.Drawing.Point(21, 28);
+            this.lbl_usak.Name = "lbl_usak";
+            this.lbl_usak.Size = new System.Drawing.Size(207, 37);
+            this.lbl_usak.TabIndex = 1;
+            this.lbl_usak.Text = "UŞAK, TÜRKİYE";
+            // 
+            // lbl_state
+            // 
+            this.lbl_state.AutoSize = true;
+            this.lbl_state.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_state.Location = new System.Drawing.Point(265, 184);
+            this.lbl_state.Name = "lbl_state";
+            this.lbl_state.Size = new System.Drawing.Size(78, 35);
+            this.lbl_state.TabIndex = 2;
+            this.lbl_state.Text = "00.00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(21, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 35);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Bugün";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(334, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "o";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(349, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "C";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(22, 113);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(206, 124);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label4.Location = new System.Drawing.Point(21, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 28);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Rüzgar:";
+            // 
+            // lbl_rzg
+            // 
+            this.lbl_rzg.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_rzg.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lbl_rzg.Location = new System.Drawing.Point(109, 279);
+            this.lbl_rzg.Name = "lbl_rzg";
+            this.lbl_rzg.Size = new System.Drawing.Size(82, 28);
+            this.lbl_rzg.TabIndex = 8;
+            this.lbl_rzg.Text = "R";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label6.Location = new System.Drawing.Point(21, 319);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 28);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Nem:";
+            // 
+            // lbl_nem
+            // 
+            this.lbl_nem.AutoSize = true;
+            this.lbl_nem.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_nem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lbl_nem.Location = new System.Drawing.Point(109, 319);
+            this.lbl_nem.Name = "lbl_nem";
+            this.lbl_nem.Size = new System.Drawing.Size(27, 28);
+            this.lbl_nem.TabIndex = 10;
+            this.lbl_nem.Text = "N";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label5.Location = new System.Drawing.Point(21, 358);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 28);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Yağış:";
+            // 
+            // lbl_ygs
+            // 
+            this.lbl_ygs.AutoSize = true;
+            this.lbl_ygs.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_ygs.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lbl_ygs.Location = new System.Drawing.Point(109, 358);
+            this.lbl_ygs.Name = "lbl_ygs";
+            this.lbl_ygs.Size = new System.Drawing.Size(24, 28);
+            this.lbl_ygs.TabIndex = 12;
+            this.lbl_ygs.Text = "Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(433, 397);
+            this.Controls.Add(this.lbl_ygs);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbl_nem);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbl_rzg);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_state);
+            this.Controls.Add(this.lbl_usak);
+            this.Controls.Add(this.lbl_temp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_temp;
+        private System.Windows.Forms.Label lbl_usak;
+        private System.Windows.Forms.Label lbl_state;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_rzg;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbl_nem;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_ygs;
+    }
+}
+
---
+++ WeatherApi/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WeatherApi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string api = "afbacc67a05b6756487b2ba103ef34bf";
+            string connection = "http://api.openweathermap.org/data/2.5/weather?q=uşak&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument weather = XDocument.Load(connection);
+            var temp = weather.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            var speed = weather.Descendants("speed").ElementAt(0).Attribute("value").Value;
+            var nem = weather.Descendants("humidity").ElementAt(0).Attribute("value").Value;
+
+            var weatherstate = weather.Descendants("weather").ElementAt(0).Attribute("value").Value;
+            lbl_temp.Text = temp;
+            lbl_rzg.Text = speed + " m/s";
+            lbl_nem.Text = nem + " %";
+            lbl_ygs.Text = "0 %";
+            lbl_state.Text = weatherstate.ToUpper();
+        }
+    }
+}
